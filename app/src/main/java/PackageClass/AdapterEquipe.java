dd7eaf0c16eb134827b0fcf02dfd8b4bad3de22c
Boaz <49524886+Brocolioze@users.noreply.github.com>
2024-02-25 18:54:29 -0500

    maj

--- app/src/main/java/PackageClass/AdapterEquipe.java
+++ app/src/main/java/PackageClass/AdapterEquipe.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class AdapterEquipe extends RecyclerView.Adapter<AdapterEquipe.ViewHolderEquipe> {
 
-    // Interface pour gérer les clics sur les éléments de RecyclerView
+
     public interface InterfaceEquipe {
         void gestionClick(int position, Equipe equipe);
     }
@@ -24,13 +24,13 @@ public class AdapterEquipe extends RecyclerView.Adapter<AdapterEquipe.ViewHolder
     private InterfaceEquipe interfaceEquipe;
     private List<Equipe> liste;
 
-    // Constructeur prenant la liste d'équipes et l'interface comme paramètres
+
     public AdapterEquipe(List<Equipe> liste, InterfaceEquipe interfaceEquipe) {
         this.liste = liste;
         this.interfaceEquipe = interfaceEquipe;
     }
 
-    // Méthode appelée lorsqu'un ViewHolder doit être créé
+
     @NonNull
     @Override
     public ViewHolderEquipe onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -39,7 +39,7 @@ public class AdapterEquipe extends RecyclerView.Adapter<AdapterEquipe.ViewHolder
         return new ViewHolderEquipe(view);
     }
 
-    // Méthode appelée pour afficher les données à une position spécifique
+
     @Override
     public void onBindViewHolder(@NonNull ViewHolderEquipe holder, int position) {
         Equipe equipe = liste.get(position);
@@ -48,36 +48,36 @@ public class AdapterEquipe extends RecyclerView.Adapter<AdapterEquipe.ViewHolder
         Picasso.get().load(equipe.getLogo()).into(holder.ivImg);
     }
 
-    // Méthode renvoyant le nombre d'éléments dans la liste
+
     @Override
     public int getItemCount() {
         return liste.size();
     }
 
-    // Classe ViewHolder pour les éléments de l'équipe
+
     public class ViewHolderEquipe extends RecyclerView.ViewHolder implements View.OnClickListener {
         public ImageView ivImg;
         public TextView idEquipe;
         public TextView nomEquipe;
 
-        // Constructeur ViewHolderEquipe
+
         public ViewHolderEquipe(@NonNull View itemView) {
             super(itemView);
             ivImg = itemView.findViewById(R.id.logoEquipe);
             idEquipe = itemView.findViewById(R.id.idEquipe);
             nomEquipe = itemView.findViewById(R.id.nomEquipe);
 
-            // Définition du listener de clic sur l'élément de vue
+
             itemView.setOnClickListener(this);
         }
 
-        // Méthode appelée lorsqu'un élément de la liste est cliqué
+
         @Override
         public void onClick(View view) {
             int position = getAdapterPosition();
             if (position != RecyclerView.NO_POSITION) {
                 Equipe equipe = liste.get(position);
-                // Vérification que l'interface n'est pas nulle avant d'appeler sa méthode
+
                 if (interfaceEquipe != null) {
                     interfaceEquipe.gestionClick(position, equipe);
                 }
--- app/src/main/java/com/example/labo_02fb/MainActivity2.java
+++ app/src/main/java/com/example/labo_02fb/MainActivity2.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
@@ -14,7 +13,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,15 +21,11 @@ import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
-import java.lang.ref.Reference;
-import java.util.ArrayList;
 import java.util.List;
 
-import PackageClass.AdapterEquipe;
 import PackageClass.AdapterJoueur;
-import PackageClass.Equipe;
-import PackageClass.Joueur;
 import PackageClass.InterfaceUtilisateur;
+import PackageClass.Joueur;
 import PackageClass.RetrofitInstance;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -46,42 +40,35 @@ public class MainActivity2 extends AppCompatActivity {
 
     int matricule_equipe;
 
-    EditText nom_ajout_joueur,prenom_ajout_joueur;
+    EditText nom_ajout_joueur, prenom_ajout_joueur;
 
     FloatingActionButton idBack;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-
         LogoEquipe = findViewById(R.id.LogoEquipe);
         EquipeNom = findViewById(R.id.EquipeNom);
-
         idBack = findViewById(R.id.idBack);
-
         rvListeJoueur = findViewById(R.id.rvListeJoueur);
         rvListeJoueur.setHasFixedSize(true);
         rvListeJoueur.setLayoutManager(new GridLayoutManager(this, 2));
 
+        View layout_carte = getLayoutInflater().inflate(R.layout.layout_carte, null);
+        nom_ajout_joueur = layout_carte.findViewById(R.id.nom_ajout_joueur);
+        prenom_ajout_joueur = layout_carte.findViewById(R.id.prenom_ajout_joueur);
 
         Intent intent = getIntent();
-
 
         idBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 setResult(RESULT_CANCELED);
                 finish();
-
             }
-
-
         });
-
 
         if (intent != null && intent.hasExtra("equipe_id") && intent.hasExtra("equipe_logo") && intent.hasExtra("equipe_nom")) {
             int equipeId = intent.getIntExtra("equipe_id", -1);
@@ -89,21 +76,15 @@ public class MainActivity2 extends AppCompatActivity {
             matricule_equipe = equipeId;
 
             String logo = intent.getStringExtra("equipe_logo");
-
             String nomEquipe = intent.getStringExtra("equipe_nom");
 
-
             Picasso.get().load(logo).into(LogoEquipe);
-
             EquipeNom.setText(nomEquipe);
 
             getListeJoueurs(equipeId);
-
-
         }
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -111,105 +92,83 @@ public class MainActivity2 extends AppCompatActivity {
         inflater.inflate(R.menu.menu_activity2, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        if(item.getItemId() == R.id.item_ajouter){
-
+        if (item.getItemId() == R.id.item_ajouter) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setCancelable(false);
-            View view = getLayoutInflater().inflate(R.layout.layout_carte,null);
+            View view = getLayoutInflater().inflate(R.layout.layout_carte, null);
             builder.setView(view);
             builder.setPositiveButton("Ajouter", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-
                     gestionClik();
-
-
                 }
             });
 
             AlertDialog alertDialog1 = builder.create();
             alertDialog1.show();
-             Toast.makeText(this,"Joueur Ajouter",Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Joueur Ajouté", Toast.LENGTH_LONG).show();
             return true;
-
         }
-
         return false;
-
     }
 
-
     private void getListeJoueurs(int equipe_ident) {
-
         InterfaceUtilisateur serveur = RetrofitInstance.getInstance().create(InterfaceUtilisateur.class);
-
         Call<List<Joueur>> call = serveur.getIdEquipe(equipe_ident);
 
         call.enqueue(new Callback<List<Joueur>>() {
             @Override
             public void onResponse(Call<List<Joueur>> call, Response<List<Joueur>> response) {
                 if (response.isSuccessful()) {
-
                     List<Joueur> liste = response.body();
-
-
                     adapterJoueur = new AdapterJoueur(liste);
-
                     rvListeJoueur.setAdapter(adapterJoueur);
-
                 } else {
                     Toast.makeText(MainActivity2.this, "Une erreur", Toast.LENGTH_LONG).show();
-
                 }
             }
 
             @Override
             public void onFailure(Call<List<Joueur>> call, Throwable t) {
-
                 Toast.makeText(MainActivity2.this, "Une erreur", Toast.LENGTH_LONG).show();
-
-
             }
         });
-
-
     }
 
-    public void gestionClik(){
+    public void gestionClik() {
+
+        String nom = nom_ajout_joueur.getText().toString().trim();
+        String prenom = prenom_ajout_joueur.getText().toString().trim();
 
 
-        if(nom_ajout_joueur.getText().toString().trim().isEmpty() || prenom_ajout_joueur.getText().toString().trim().isEmpty()){
-
-            Toast.makeText(MainActivity2.this,"Veuillez entrer les champs",Toast.LENGTH_LONG).show();
-
-        }else{
-
-            InterfaceUtilisateur serveur = RetrofitInstance.getInstance().create(InterfaceUtilisateur.class);
-
-            Call<Boolean> call = serveur.addJoueur(nom_ajout_joueur.getText().toString(),prenom_ajout_joueur.getText().toString(),matricule_equipe);
-
-
-            call.enqueue(new Callback<Boolean>() {
-                @Override
-                public void onResponse(Call<Boolean> call, Response<Boolean> response) {
-                    boolean resultat = response.body();
-                }
-
-                @Override
-                public void onFailure(Call<Boolean> call, Throwable t) {
-                    Toast.makeText(MainActivity2.this,"Une erreur",Toast.LENGTH_LONG).show();
-                }
-            });
+        if (nom.isEmpty() || prenom.isEmpty()) {
+            Toast.makeText(MainActivity2.this, "Veuillez remplir tous les champs", Toast.LENGTH_LONG).show();
+            return; // Sortir de la méthode si les champs sont vides
         }
 
 
+        InterfaceUtilisateur serveur = RetrofitInstance.getInstance().create(InterfaceUtilisateur.class);
+        Call<Boolean> call = serveur.addJoueur(nom, prenom, matricule_equipe);
 
+        call.enqueue(new Callback<Boolean>() {
+            @Override
+            public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                if (response.isSuccessful() && response.body() != null && response.body()) {
+                    Toast.makeText(MainActivity2.this, "Joueur ajouté avec succès", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity2.this, "Erreur lors de l'ajout du joueur", Toast.LENGTH_LONG).show();
+                }
+            }
 
+            @Override
+            public void onFailure(Call<Boolean> call, Throwable t) {
+                Toast.makeText(MainActivity2.this, "Une erreur est survenue : " + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
-
 
 
 }
